7a5f44b76b23b9672daafc9d40f07c1085518b5f
yasminfrr11 <yasminfbarros8@gmail.com>
2026-03-15 11:56:11 -0300

    added collision + need to fix error when dropping a block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from copy import deepcopy
+from random import choice, randrange
 
 w, h = 10, 20
 tile = 45
@@ -11,7 +13,7 @@ clock = pygame.time.Clock()
 
 grid = [pygame.Rect(x*tile, y*tile, tile, tile) for x in range(w) for y in range(h)]
 
-# made considering that the blocks are formed by coordinates in a cartesian plane (each pixel is 1`cm`)
+# made considering that the blocks are formed by coordinates in a cartesian plane (each pixel is 1'cm')
 blocks_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
             [(0, -1), (-1, -1), (-1, 0), (0, 0)],
             [(-1, 0), (-1, 1), (0, 0), (0, -1)],
@@ -22,8 +24,18 @@ blocks_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
 
 blocks = [[pygame.Rect(x + w // 2, y + 1, 1, 1) for x, y in block_pos] for block_pos in blocks_pos]
 block_rect = pygame.Rect(0, 0, tile - 2, tile - 2)
+field = [[0 for i in range(w)] for i in range(h)]
 
-block = blocks[0]
+animation_count, animation_speed, animation_limit = 0, 10, 2000 #blocks falling animation
+
+block = deepcopy(choice(blocks)) # choice will select a random block pattern
+
+def checkBorders(): #blocks will collide with the borders
+    if block[i].x < 0 or block[i].x > w - 1:
+        return False
+    elif block[i].y > h - 1 or field[block[i].y][block[i].x]:
+        return False
+    return True
 
 while True:
     dx = 0
@@ -38,10 +50,30 @@ while True:
                 dx = -1
             elif event.key == pygame.K_RIGHT:
                 dx = 1
-
+            elif event.key == pygame.K_DOWN:
+                animation_limit = 100
     # x
+    block_copy = deepcopy(block)
     for i in range(4):
         block[i].x += dx
+        if not checkBorders():
+            block = deepcopy(block_copy)
+            break
+
+    # y
+    animation_count += animation_speed
+    if animation_count > animation_limit:
+        animation_count = 0
+        block_copy = deepcopy(block)
+        for i in range(4):
+            block[i].y += 1
+        if not checkBorders():
+            for i in range(4):
+                field[block_copy[i].y][block_copy[i].x] = pygame.Color('purple')
+            block = deepcopy(choice(blocks))
+            animation_limit = 120
+            break
+
 
     #background grid draw    
     [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
@@ -51,6 +83,13 @@ while True:
         block_rect.x = block[i].x * tile
         block_rect.y = block[i].y * tile
         pygame.draw.rect(game_sc, pygame.Color('purple'), block_rect)
+
+    # field draw
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                block_rect.x, block_rect.y = x * tile, y * tile
+                pygame.draw.rect(game_sc, col, block_rect)
 
     pygame.display.flip()
     clock.tick()
